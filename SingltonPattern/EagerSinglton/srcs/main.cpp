d21fbdfc49cc079a62667682b9d96a2bf9cd32d4
yumzhana <yumyumzhana@gmail.com>
2025-08-13 19:18:26 +0200

    Singleton pattern

---
+++ SingltonPattern/EagerSinglton/srcs/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytsyrend <ytsyrend@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 18:41:01 by ytsyrend          #+#    #+#             */
+/*   Updated: 2025/08/13 18:34:16 by ytsyrend         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ChocolateBoiler.class.hpp"
+
+int main() 
+{
+    ChocolateBoiler* boiler1 = ChocolateBoiler::getInstance();
+    boiler1->fill();
+    boiler1->boil();
+
+    ChocolateBoiler* boiler2 = ChocolateBoiler::getInstance();
+    std::cout << "Is boiler2 boiled? " << boiler2->isBoiled() << std::endl;
+    delete boiler1;
+    return 0;
+}
---
+++ SingltonPattern/MayerLazySinglton/srcs/ChocolateBoiler.class.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ChocolateBoiler.class.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ytsyrend <ytsyrend@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 18:24:16 by ytsyrend          #+#    #+#             */
+/*   Updated: 2025/08/13 18:10:46 by ytsyrend         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ChocolateBoiler.class.hpp"
+
+/****************************************************/
+/*                    Constructor                   */
+/****************************************************/
+
+ChocolateBoiler::ChocolateBoiler(void) 
+{
+    empty = true;
+    boiled = false;
+    if (DEBUG){ std::cout << GREEN << "[ChocolateBoiler] Default Constructor called" << RESET_COLOR << std::endl;}
+}
+
+/****************************************************/
+/*                    Destructor.                   */
+/****************************************************/
+
+ChocolateBoiler::~ChocolateBoiler(void) 
+{
+    if (DEBUG){std::cout << GREEN << "[ChocolateBoiler] Destructor called" << RESET_COLOR << std::endl;}
+}
+
+/****************************************************
+*                 Memeber Functions                *
+****************************************************/
+
+ChocolateBoiler& ChocolateBoiler::getInstance()
+{
+    static ChocolateBoiler instance;
+    return instance;
+}
+
+void ChocolateBoiler::fill()
+{
+    if(this->isEmpty())
+    {
+        empty = false;
+        boiled = false;
+    }
+}
+
+void ChocolateBoiler::drain()
+{
+    if( !this->isEmpty() && this->isEmpty())
+    {
+        empty = true;
+    }
+}
+
+void ChocolateBoiler::boil()
+{
+    if( !this->isEmpty() && !this->isEmpty())
+    {
+        boiled = true;
+    }
+}
+
+bool ChocolateBoiler::isEmpty()
+{
+    return (this->empty);
+}
+
+bool ChocolateBoiler::isBoiled()
+{
+    return (this->boiled);
+}
